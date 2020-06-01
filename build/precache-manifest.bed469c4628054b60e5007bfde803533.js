9eeaf4c2fb51f1f5ab8b74f967d1931c56a72721
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-06-01 14:07:39 -0500

    deleted some files in .gitignore

---
+++ build/precache-manifest.bed469c4628054b60e5007bfde803533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ca11ad7a5ac46ec1996ca09cf32505",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17a975a0b11417bdd0c8",
+    "url": "/static/css/2.553a2253.chunk.css"
+  },
+  {
+    "revision": "17a975a0b11417bdd0c8",
+    "url": "/static/js/2.382eea16.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.382eea16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e570e745f21c833f17d2",
+    "url": "/static/js/main.01c00feb.chunk.js"
+  },
+  {
+    "revision": "39d18ae026459b2873d4",
+    "url": "/static/js/runtime-main.329d9675.js"
+  }
+]);
